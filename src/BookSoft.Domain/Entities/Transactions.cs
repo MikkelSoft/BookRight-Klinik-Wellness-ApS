5488cde9c4f29832b7e8bd222467b6da556ca3e4
sebastex <bash1010jan@gmail.com>
2026-05-15 01:57:55 +0200

    added transactions studying how blazor fits into all this now and looking into clear architecture

--- src/BookSoft.Domain/Entities/Transactions.cs
+++ src/BookSoft.Domain/Entities/Transactions.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using BookSoft.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BookSoft.Domain.Entities
 {
-    public class Transactions
+    public class Transactions : AggregateRoot
     {
+        public DateTime TransactionDate { get; private set; }
+        public AppointmentType AppointmentType { get; private set; }
+        public Practitioner Practitioners { get; private set; }
+        public FullName PractiionerName => Practitioners.FullName;
+
+        public decimal cost => AppointmentType switch
+        {
+            AppointmentType.Consultation => 300,
+            AppointmentType.Checkup => 200,
+            AppointmentType.Procedure => 600,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
     }
 }
--- src/BookSoft.Infrastructure/Data/BookSoftDbContext.cs
+++ src/BookSoft.Infrastructure/Data/BookSoftDbContext.cs
@@ -11,18 +11,21 @@ namespace BookSoft.Infrastructure.Data
         public DbSet<Practitioner> Practitioners { get; set; } //opretter kolonner i db
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost;Database=BookSoftDB;Trusted_Connection=True;TrustServerCertificate=True");
         }
-
+        //have not added transaction here because it is not needed for the many to many relationship between practitioner and appointment, and patient and appointment. It is only needed for the one to many relationship between patient and transaction, but that is not the focus of this project, so I have left it out for now.
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfigurationsFromAssembly(
                 typeof(BookSoftDbContext).Assembly);
+
 
             modelBuilder.Entity<Practitioner>()
                 .HasMany(e => e.Appointments)
