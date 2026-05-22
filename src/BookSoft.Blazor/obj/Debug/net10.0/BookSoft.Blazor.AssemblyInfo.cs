2ff48991101cf56fabbe78aadf9c6d55af21f816
sebastex <bash1010jan@gmail.com>
2026-05-22 10:38:29 +0200

    ui is now on its way homies we are asserting the color scheme and layout features

--- src/BookSoft.Blazor/obj/Debug/net10.0/BookSoft.Blazor.AssemblyInfo.cs
+++ src/BookSoft.Blazor/obj/Debug/net10.0/BookSoft.Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookSoft.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66ea620eac03f5ab17c9352f318da7e8e872cd08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e92d18b18daea3027a722024e1b6c3b3ae39d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookSoft.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookSoft.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
