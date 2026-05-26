6ea7bfeee5361f81690db9b21fa3cfaa726e2cbf
MIKKELSUPERSPORT <mihe74600@edu.ucl.dk>
2026-05-26 13:47:54 +0200

    Flyttet enums til de rette foldere, har oprettet transactions i EF

--- tests/BookSoft.UseCases.Tests/obj/Debug/net10.0/BookSoft.UseCases.Tests.AssemblyInfo.cs
+++ tests/BookSoft.UseCases.Tests/obj/Debug/net10.0/BookSoft.UseCases.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookSoft.UseCases.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ff87e3ab93f0a144ea527e3f50a80c5572ebb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130e060cce84386857dc74393ad9a57060b13f3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookSoft.UseCases.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookSoft.UseCases.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
